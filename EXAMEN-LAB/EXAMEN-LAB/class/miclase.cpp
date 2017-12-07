97bfa5552e55d133dcfb9f5d17be5fcef95f7a18
Alex <aparedese@upao.edu.pe>
2017-12-07 17:17:41 -0500

    nuevas clases

---
+++ EXAMEN-LAB/EXAMEN-LAB/class/miclase.cpp
@@ -0,0 +1,9 @@
+//
+//  miclase.cpp
+//  EXAMEN-LAB
+//
+//  Created by Alex Paredes on 7/12/17.
+//  Copyright © 2017 Alex Paredes. All rights reserved.
+//
+
+#include "miclase.hpp"
---
+++ EXAMEN-LAB/EXAMEN-LAB/class/miclase.hpp
@@ -0,0 +1,14 @@
+//
+//  miclase.hpp
+//  EXAMEN-LAB
+//
+//  Created by Alex Paredes on 7/12/17.
+//  Copyright © 2017 Alex Paredes. All rights reserved.
+//
+
+#ifndef miclase_hpp
+#define miclase_hpp
+
+#include <stdio.h>
+
+#endif /* miclase_hpp */
