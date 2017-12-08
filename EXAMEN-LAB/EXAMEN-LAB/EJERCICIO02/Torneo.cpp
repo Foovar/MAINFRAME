c18e1306e968db2d91f3a0ef9d34038e6ab7f426
Alex <aparedese@upao.edu.pe>
2017-12-07 20:51:21 -0500

    Avanzando con el examen

--- EXAMEN-LAB/EXAMEN-LAB/EJERCICIO02/Torneo.cpp
+++ EXAMEN-LAB/EXAMEN-LAB/EJERCICIO02/Torneo.cpp
@@ -13,6 +13,17 @@ TorneoDelPoder::TorneoDelPoder(){
     this->grupos.push_back(new Grupo('B', "Grupo B"));
     this->grupos.push_back(new Grupo('C', "Grupo C"));
     this->NuevoParticipante("Goku", 10000);
+    this->NuevoParticipante("Krillin", 1000);
+    this->NuevoParticipante("Cell", 12000);
+    this->NuevoParticipante("Piccolo", 5000);
+    this->NuevoParticipante("Gohan", 5000);
+    this->NuevoParticipante("Mr. Satan", 999999);
+    this->NuevoParticipante("Nro. 18", 6000);
+    this->NuevoParticipante("Nro. 17", 6000);
+    this->NuevoParticipante("Yamcha", 3);
+    this->NuevoParticipante("Vegeta", 9000);
+    this->NuevoParticipante("Frezer", 8500);
+    this->NuevoParticipante("Maestro Roshi", 4000);
 }
 
 void TorneoDelPoder::NuevoParticipante(std::string nombre, int ki){
